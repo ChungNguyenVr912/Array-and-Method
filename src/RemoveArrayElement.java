00de66493828726dd7ebb2026241417260a21e70
ChungNguyenVr912 <126665381+ChungNguyenVr912@users.noreply.github.com>
2023-04-04 14:32:19 +0700

    Create RemoveArrayElement.java

---
+++ src/RemoveArrayElement.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class RemoveArrayElement {
+    public static void main(String[] args) {
+        int[] arr = {5, 6, 8, 2, 3, 1, 0, 1, 5, 8, 9};
+        for(int k : arr){
+            System.out.print(k + "\t");
+        }
+        System.out.println();
+        boolean exist = false;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number u want to remove: ");
+        int number = scanner.nextInt();
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == number){
+                System.out.println("Found number at index: "+ i);
+                exist = true;
+                for(int j = i; j < arr.length - 1; j++){
+                    arr[j] = arr[j+1];
+                }
+                arr[arr.length-1] = 0;
+                i--;
+            }
+        }
+        System.out.printf("Array after %d removed: \n", number);
+        for (int k: arr) {
+            System.out.print(k + "\t");
+        }
+    }
+}
